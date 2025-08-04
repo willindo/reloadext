fc99cee19834d3e0743690d2bb63345608b39d2e
willindo <badshanoordeen@gmail.com>
2025-08-04 09:45:49 +0530

    link

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,10 +1,16 @@
+import Link from "next/dist/client/link";
+
+
 export default function Home() {
   return (
     <main  >
-      <h1 className="text-red-500  border-red-500 font-bold">🚀 DevOps Project Frontend</h1>
-      <h3 className="text-xl">i use multi command prompt for reloading concern</h3>
-      <p>This is the default landing page of your Next.js app.</p>
-      <h2> now front and back are working together</h2>
+       <h1 className="text-3xl font-bold">Welcome to Reload Ops</h1>
+
+      <div className="space-y-2">
+        <Link href="/login" className="text-blue-500 underline">Login</Link>
+        <Link href="/register" className="text-blue-500 underline">Register</Link>
+        <Link href="/products" className="text-blue-500 underline">View Products</Link>
+      </div>
     </main>
   );
 }
